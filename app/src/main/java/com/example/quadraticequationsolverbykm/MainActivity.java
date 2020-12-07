d4811697279e159349b10b09d6feff50b49c6f0a
lkjuhkmnop <maksimkide@gmail.com>
2020-12-07 18:33:32 +0300

    empty strings => 0

--- app/src/main/java/com/example/quadraticequationsolverbykm/MainActivity.java
+++ app/src/main/java/com/example/quadraticequationsolverbykm/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.quadraticequationsolverbykm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,9 +35,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String t1 = ed1.getText().toString();
         String t2 = ed2.getText().toString();
         String t3 = ed3.getText().toString();
-        double a = Double.parseDouble(t1);
-        double b = Double.parseDouble(t2);
-        double c = Double.parseDouble(t3);
+//        Log.d("MyDebug", "t1: " + t1 + "; t2: " + t2 + "; t3: " + t3.equals(""));
+        double a, b, c;
+        if (t1.equals("")) {
+            a = 0;
+        } else {
+            a = Double.parseDouble(t1);
+        }
+        if (t2.equals("")) {
+            b = 0;
+        } else {
+            b = Double.parseDouble(t2);
+        }
+        if (t3.equals("")) {
+            c = 0;
+        } else {
+            c = Double.parseDouble(t3);
+        }
 
         if (a == 0) {
             result.setText("a = 0 !!!");
@@ -46,7 +61,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 result.setText("no solution");
             } else if (D == 0) {
                 double x1 = (-1 * b + Math.sqrt(D)) / (2 * a);
-                result.setText(String.valueOf(x1));
+//                result.setText(String.valueOf(x1));
+                result.setText(String.format(Locale.US, "%.3f", x1));
             } else {
                 double x1 = (-1 * b + Math.sqrt(D)) / (2 * a);
                 double x2 = (-1 * b - Math.sqrt(D)) / (2 * a);
